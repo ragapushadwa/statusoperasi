925caf42c6d69b6890b363444fceed25210cff80
ragapushadwa <ragapushadwa@gmail.com>
2020-02-25 15:35:20 +0700

    Commit takut ilang

--- simulasistatus.py
+++ simulasistatus.py
@@ -4,9 +4,9 @@ import random
 import time
 
 while True:
-    Daya= str(random.randrange(50))
-    LajuAlir= str(random.randrange(50))
-    Suhu= str(random.randrange(50))
+    Daya= str(random.randrange(100))
+    LajuAlir= str(random.randrange(100))
+    Suhu= str(random.randrange(100))
     g= requests.get("http://localhost/statusoperasi/datastatus.php?Daya="+Daya+"&LajuAlir="+LajuAlir+"&Suhu="+Suhu)
     print(g.text)
     time.sleep(1)
